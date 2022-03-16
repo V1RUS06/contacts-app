7265045326fab4e40ec4e8738fc56acddaa6859a
Denis <denison4@mail.ru>
2022-03-16 11:30:37 +0300

    поиск контактов

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 import ContactCard from "../cards/ContactCard";
 import {TransitionGroup, CSSTransition} from "react-transition-group";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
@@ -16,6 +16,7 @@ const ContactList = () => {
   const [editModal, setEditModal] = useState<boolean>(false)
   const [editContact, setEditContact] = useState<null | IUserContact>(null)
   const {deleteContact, deleteUserContact} = useActions()
+  const [searchValue, setSearchValue] = useState<string>('')
 
 
   const remove = (id: string | number | undefined) => {
@@ -23,19 +24,21 @@ const ContactList = () => {
     deleteContact(newContactList)
     deleteUserContact({...user, contacts: newContactList})
   }
-  const edit = (id: string ) => {
+  const edit = (id: string) => {
     const editContact = user.contacts.find(contact => contact.id === id)
 
-    if(editContact){
+    if (editContact) {
       setEditContact(editContact)
       setEditModal(true)
     }
   }
 
+  const searchList = user.contacts?.filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+
   return (
     <div className="contacts__container">
       <div className="contacts__btn">
-        <MyButton  onClick={() => setModal(true)}>
+        <MyButton onClick={() => setModal(true)}>
           Добавить контакт
         </MyButton>
       </div>
@@ -49,25 +52,35 @@ const ContactList = () => {
       </Modal>
 
       <div>
-        <input type="text" placeholder="Поиск по имени" className="input"/>
+        <input
+          type="text"
+          placeholder="Поиск по имени"
+          className="input"
+          onChange={e => setSearchValue(e.target.value)}/>
       </div>
 
       <h1 className="title">
         Список контактов
       </h1>
+      {searchList?.length ?
+        <TransitionGroup>
+          {searchList?.map(user =>
+            <CSSTransition
+              key={user.id}
+              timeout={500}
+              classNames="post"
+            >
+              <ContactCard key={user.id} name={user.name} phone={user.phone} remove={() => remove(user.id)}
+                           edit={() => edit(user.id)}/>
+            </CSSTransition>
+          )
 
-      <TransitionGroup>
-        {user.contacts?.map(user =>
-          <CSSTransition
-            key={user.id}
-            timeout={500}
-            classNames="post"
-          >
-          <ContactCard key={user.id} name={user.name} phone={user.phone} remove={()  => remove(user.id)} edit={()  => edit(user.id)} />
-          </CSSTransition>
-
-        )}
-      </TransitionGroup>
+          }
+        </TransitionGroup>
+        : <div style={{height: 200, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <h1>Контакта с таким именем не существует!</h1>
+        </div>
+      }
     </div>
   );
 };
